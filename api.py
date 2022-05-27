5b01e6ba5d179ff2eac377ba11087073f3a686d8
merken <merken.maarten@gmail.com>
2022-05-27 14:23:55 +0200

    Fix

--- api.py
+++ api.py
@@ -22,6 +22,9 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/search', methods=['POST'])
+def search():
+    return render_template('index.html')
 
 # @app.route('/search', methods=['POST'])
 # def search():
